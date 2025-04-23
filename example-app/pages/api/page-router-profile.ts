81dcf47a2ef8f50a90a2eb6cd0d7c48eb081b445
Code Region <48864014+Challenge-Guy@users.noreply.github.com>
2025-04-23 07:33:00 -0300

    Update page-router-profile.ts

--- example-app/pages/api/page-router-profile.ts
+++ example-app/pages/api/page-router-profile.ts
@@ -1,8 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { pageRouterAuth } from '../../lib/auth0';
+export default pageRouterAuth.withApiAuthRequired(async function profile(req: NextApiRequest, res: NextApiResponse) {
+  const session = await pageRouterAuth.getSession(req, res);
 
-export default pageRouterAuth.withApiAuthRequired(async function profile(req: NextApiRequest, res: NextApiResponse) { 
-  const session = await pageRouterAuth.getSession(req, res); 
- 
-  res.status(200).json(session?.user); 
-}); 
+  res.status(200).json(session?.user);
+});
